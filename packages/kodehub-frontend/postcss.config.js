70fb108664602e21b1ca6d86e62e2029034b18a2
dwikiarlan <arrlancore@gmail.com>
2020-03-12 06:52:58 +0700

    fix css build

--- packages/kodehub-frontend/postcss.config.js
+++ packages/kodehub-frontend/postcss.config.js
@@ -1,53 +1,20 @@
-// module.exports = {
-//     plugins: [
-//         "postcss-import",
-//         "tailwindcss",
-//         "autoprefixer"
-//     ]
-// };
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './src/shared/components/**/*.{js,jsx,ts,tsx}'
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+];
 
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-
-const prod = process.env.NODE_ENV === 'production';
 module.exports = {
   plugins: [
+    'postcss-import',
     'tailwindcss',
-    prod ? 'autoprefixer' : null,
-    prod
-      ? [
-          'cssnano',
-          {
-            preset: 'default'
-          }
-        ]
-      : null,
-    prod
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './src/shared/components/**/*.{js,jsx,ts,tsx}'
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-          }
-        ]
-      : null,
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
     'postcss-preset-env'
   ]
-  // webpack: function (config) {
-  //     config.module.rules.push({
-  //         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-  //         use: {
-  //             loader: 'url-loader',
-  //             options: {
-  //                 limit: 100000,
-  //                 name: '[name].[ext]'
-  //             }
-  //         }
-  //     })
-  //     return config
-  // }
 };
